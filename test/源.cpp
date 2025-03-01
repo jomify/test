be81d2bca03edd0b157a87a29e2faeffa791c419
Jomify <neothype@163.com>
2025-03-01 19:44:53 +0800

    hehe

--- test/源.cpp
+++ test/源.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
 using namespace std;
+class people {
+public:
+	static int a;
+	int b;
+	virture people() {} = 0;
+	virture ~people() {} = 0;
+
+};
+class peoson :people {
+	person(int a, int b) :a(a), b(b) {}
+	int* p;
+	peoson(const peoson& p) {
+		this->a = p.a;
+		this->b = p.b;
+		this->p = new int(*p.p);
+	}
+
+};
 int main() {
 	cout << "²âÊÔ³É¹¦" << endl;
 	return 0;
