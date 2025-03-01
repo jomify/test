e943eab20fb80ba8ce8ae4e14f298f2b60d7a0ab
jomify <neothype@163.com>
2025-03-01 20:28:55 +0800

    Update 源.cpp

--- test/源.cpp
+++ test/源.cpp
@@ -8,19 +8,7 @@ public:
 	virture ~people() {} = 0;
 
 };
-class peoson :people {
-	person(int a, int b) :a(a), b(b) {}
-	int* p;
-	peoson(const peoson& p) {
-		this->a = p.a;
-		this->b = p.b;
-		this->p = new int(*p.p);
-	}
 
-};
-class guit:public peoson{
-
-}
 int main() {
 	cout << "²âÊÔ³É¹¦" << endl;
 	return 0;
